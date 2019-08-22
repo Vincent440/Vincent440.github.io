87ec1a2dbe1049919624ab909384d782117c8e72
vince s <vinceshury@gmail.com>
2019-08-22 13:06:35 -0400

    updated web urls and links , along with project names

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,7 @@ $(()=>{
     "https://github.com/mike14747/LocalEyes",true,"https://mike14747.github.io/LocalEyes/");
 
     const eMarketplace = new App("eMarketplace","./assets/images/apps/emarketplace.png",
-    "https://github.com/letscook1/project2", true,"https://glacial-bayou-58542.herokuapp.com/");
+    "https://github.com/Vincent440/emarketplace", true,"https://emarketplace.herokuapp.com/");
 
     const techScraper = new App("Tech Scraper","./assets/images/apps/techscraper.png",
     "https://github.com/Vincent440/mongoose-cheerio-scraper",true,"https://secret-atoll-83145.herokuapp.com/");
@@ -21,8 +21,8 @@ $(()=>{
     const sequelBurger = new App("Sequelized Burger","./assets/images/apps/sequelburger.png",
     "https://github.com/Vincent440/sequelized-burger",true,"https://obscure-atoll-61774.herokuapp.com/");
 
-    const eatBurger = new App("Eat The Burger","./assets/images/apps/burgerapp.png",
-    "https://github.com/Vincent440/burger",true,"https://blooming-everglades-90768.herokuapp.com/");
+    const eatBurger = new App("MySQL Eat The Burger","./assets/images/apps/burgerapp.png",
+    "https://github.com/Vincent440/burger",true,"https://mysql-eat-the-burger-app.herokuapp.com/");
 
     const friend = new App("Friend Finder","./assets/images/apps/friendfinder.png",
     "https://github.com/Vincent440/friendfinder",true,"https://vince-friendfinder.herokuapp.com/");
