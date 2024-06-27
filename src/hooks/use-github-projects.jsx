fd0a61351e866f46c7d4b3f95635386f95ea475c
Vincent440 <vinceshury@gmail.com>
2024-06-27 19:17:38 -0400

    Formated `use-github-projects`

--- src/hooks/use-github-projects.jsx
+++ src/hooks/use-github-projects.jsx
@@ -1,31 +1,29 @@
 import { useStaticQuery, graphql } from "gatsby"
 
 export const useGithubProjects = () => {
-  const data = useStaticQuery(
-    graphql`
-      {
-        allGithubData {
-          nodes {
-            data {
-              user {
-                pinnedItems {
-                  nodes {
-                    url
-                    homepageUrl
-                    description
-                    name
-                    id
-                    openGraphImageUrl
-                    usesCustomOpenGraphImage
-                    shortDescriptionHTML
-                  }
+  const data = useStaticQuery(graphql`
+    {
+      allGithubData {
+        nodes {
+          data {
+            user {
+              pinnedItems {
+                nodes {
+                  url
+                  homepageUrl
+                  description
+                  name
+                  id
+                  openGraphImageUrl
+                  usesCustomOpenGraphImage
+                  shortDescriptionHTML
                 }
               }
             }
           }
         }
       }
-    `
-  )
+    }
+  `)
   return data.allGithubData.nodes[0].data.user.pinnedItems.nodes
 }
