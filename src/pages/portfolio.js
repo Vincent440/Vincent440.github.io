bf4ac1910d8fd9d4b9f043e0ca0d2e109cdbb946
Vincent440 <vinceshury@gmail.com>
2024-07-02 20:58:00 -0400

    Portfolio page updated for bootstrap v5

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import Jumbotron from "react-bootstrap/Jumbotron"
 import Card from "react-bootstrap/Card"
 
 import Layout from "../components/layout"
@@ -11,26 +10,25 @@ import Projects from "../components/projects"
 const Portfolio = ({ location }) => {
   return (
     <Layout location={location}>
-      <Jumbotron fluid className="py-2 rounded">
-        <h1 className="text-center display-2">Portfolio</h1>
-      </Jumbotron>
       <Row>
         <Col>
           <Card bg="light">
             <Card.Header>
-              <Card.Title as="h2">Projects</Card.Title>
+              <Card.Title as="h1" className="display-2">
+                Portfolio Projects
+              </Card.Title>
             </Card.Header>
             <Card.Body>
-              <Card.Text className="lead">
-                Deployed applications will have links to the hosted site. Please
-                wait up to 15 seconds for the Heroku urls to load the page the
-                first time.
+              <Card.Text>
+                Deployed applications will have links to the hosted site.
+                Full-stack projects are hosted on free or low-cost cloud
+                providers, they may take extra time to load so please be
+                patient.
               </Card.Text>
-              <Card.Body>
-                <Row xs={1} sm={1} lg={2} xl={3}>
-                  <Projects />
-                </Row>
-              </Card.Body>
+
+              <Row md={1} lg={2} xl={3}>
+                <Projects />
+              </Row>
             </Card.Body>
           </Card>
         </Col>
