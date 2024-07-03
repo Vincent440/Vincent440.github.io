56299aa65c5fdba570e58a7a206ddee22e060e7d
Vincent440 <vinceshury@gmail.com>
2024-07-02 20:56:48 -0400

    Restyled projects page and updated to bootstrap 5

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,48 +12,50 @@ const Projects = () => {
   return (
     <>
       {projects.map(project => (
-        <Col key={project.id} className="my-3">
-          <Card bg="dark" text="light" className="mx-1 my-3 h-100">
+        <Col key={project.id} className="my-2">
+          <Card bg="dark" text="light" className="h-100">
             <Card.Header>
               {project.usesCustomOpenGraphImage ? (
                 <Card.Img
                   variant="top"
-                  className="img-thumbnail"
+                  className="img-thumbnail text-body-secondary"
                   src={project.openGraphImageUrl}
                   alt={`Screenshot of ${project.name}`}
                 />
               ) : (
-                <Card.Text className="text-muted text-white p-2">
+                <Card.Text className="text-body-secondary bg-secondary rounded text-center p-2">
                   Preview Image Unavailable
                 </Card.Text>
               )}
             </Card.Header>
 
             <Card.Body>
-              <Card.Title as="h3">{project.name}</Card.Title>
+              <Card.Title as="h3" className="text-capitalize text-center">
+                {project.name.replaceAll("-", " ")}
+              </Card.Title>
               <Card.Text>{project.description}</Card.Text>
             </Card.Body>
 
-            <ButtonGroup className="my-2 mx-2 px-0">
+            <ButtonGroup className="mx-2 mb-2">
               <Button
-                className="px-0 py-2"
+                className="d-flex justify-content-center align-items-center gap-2"
                 variant="primary"
                 target="_blank"
                 rel="noreferrer"
                 href={project.homepageUrl}
               >
-                <Link45deg size={26} className="mr-2" />
+                <Link45deg size={26} />
                 Website
               </Button>
               <Button
-                className="px-0 py-2"
+                className="d-flex justify-content-center align-items-center gap-2"
                 variant="secondary"
                 target="_blank"
                 rel="noreferrer"
                 href={project.url}
               >
-                <Github size={26} className="mr-2" />
-                GitHub
+                <Github size={26} />
+                <span>GitHub</span>
               </Button>
             </ButtonGroup>
           </Card>
