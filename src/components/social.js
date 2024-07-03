eac7c58c67547ca92cd1500fecfbe3b8a48624dd
Vincent440 <vinceshury@gmail.com>
2024-07-02 20:55:50 -0400

    Redesigned `Social` component

--- src/components/social.js
+++ src/components/social.js
@@ -1,49 +1,43 @@
 import React from "react"
-import Card from "react-bootstrap/Card"
 import ListGroup from "react-bootstrap/ListGroup"
 
 const Social = () => {
   return (
-    <Card>
-      <Card.Header>
-        <h3>Social Profiles</h3>
-      </Card.Header>
-      <Card.Body>
-        <ListGroup className="justify-content-center" horizontal="md">
-          <ListGroup.Item>
-            <a
-              href="https://www.linkedin.com/in/VincentShury/"
-              target="_blank"
-              rel="noreferrer"
-              referrerPolicy="no-referrer"
-            >
-              LinkedIn
-            </a>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <a
-              className="image-thumbnail"
-              href="https://www.github.com/Vincent440"
-              target="_blank"
-              rel="noreferrer"
-              referrerPolicy="no-referrer"
-            >
-              GitHub
-            </a>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <a
-              href="https://stackoverflow.com/users/story/10886832"
-              target="_blank"
-              rel="noreferrer"
-              referrerPolicy="no-referrer"
-            >
-              Stack Overflow
-            </a>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card.Body>
-    </Card>
+    <ListGroup className="d-flex justify-content-center" horizontal="sm">
+      <ListGroup.Item className="bg-dark text-center align-middle text-light">
+        <h4>Socials</h4>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <a
+          href="https://www.linkedin.com/in/VincentShury/"
+          target="_blank"
+          rel="noreferrer"
+          referrerPolicy="no-referrer"
+        >
+          LinkedIn
+        </a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <a
+          href="https://www.github.com/Vincent440"
+          target="_blank"
+          rel="noreferrer"
+          referrerPolicy="no-referrer"
+        >
+          GitHub
+        </a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <a
+          href="https://stackoverflow.com/users/story/10886832"
+          target="_blank"
+          rel="noreferrer"
+          referrerPolicy="no-referrer"
+        >
+          Stack Overflow
+        </a>
+      </ListGroup.Item>
+    </ListGroup>
   )
 }
 export default Social
